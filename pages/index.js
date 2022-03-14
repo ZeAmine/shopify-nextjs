b53c764bc88847a9b9dc83182c3de021294bb90e
Amine ZEGMOU <63230216+ZeAmine@users.noreply.github.com>
2022-03-14 17:10:01 +0100

    ⚡️

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import {getProductsInCollection} from "../lib/shopify";
+
+//components
+import ProductList from "../components/ProductList";
+
+export default function Home({ products }) {
+  return (
+    <div className="text-3xl">
+        <ProductList products={products} />
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const products = await getProductsInCollection()
+
+  return {
+    props: {products},
+  }
+}
